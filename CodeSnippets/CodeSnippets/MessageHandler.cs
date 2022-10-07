6bac10bc9c355b36fa6baa6e7553affb8c2995d5
Aram Yako <aram.yako@ibiz-solutions.se>
2022-10-07 09:39:54 +0200

    Added mock and verify httpMessageHandler

---
+++ CodeSnippets/CodeSnippets/MessageHandler.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippets
+{
+    public class MessageHandler
+    {
+
+        private void MockAndVerifyHttpMessageHandler()
+        {
+            //Mock httpClient
+            //Get posted content
+            var MockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+            string ActualHttpRequestContent = "";
+            HttpRequestMessage ActualHttpRequestMessage = null;
+
+            MockHttpMessageHandler
+            .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>(
+                    (httpRequestMessage, cancellationToken) =>
+                    {
+                        ActualHttpRequestContent = httpRequestMessage.Content
+                           .ReadAsStringAsync()
+                           .GetAwaiter()
+                           .GetResult();
+
+                        ActualHttpRequestMessage = httpRequestMessage;
+                    })
+                .Returns(
+                Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(string.Empty)
+                }));
+
+            var httpClient = new HttpClient(MockHttpMessageHandler.Object);
+
+
+            //Verify httpClient
+            MockHttpMessageHandler.Protected().Verify("SendAsync", Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                req.Method == HttpMethod.Post
+                && req.RequestUri == new Uri($"https://saintgobaindsigroupe4--pltqa.sandbox.testtesttest.salesforce.com/services/Soap/c/55.0/00D1/00511ZZZ")),
+                ItExpr.IsAny<CancellationToken>());
+        }
+    }
+}
